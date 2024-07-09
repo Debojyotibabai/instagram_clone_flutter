86a68cdfbec5cb31b77f477fee0898dbc75721fd
Debojyotibabai <debojyoti@appstango.com>
2024-07-09 10:29:16 +0530

    env added

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:instagram_clone/bloc/current_user_data/current_user_data_bloc.dart';
 import 'package:instagram_clone/router/router.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb || defaultTargetPlatform == TargetPlatform.android) {
     await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyBimt0pe5Vat6IsjA8QUHOaiaCKfwUNGwI",
-        appId: "1:881715470598:web:035a2b22c411ac8e327e87",
-        messagingSenderId: "881715470598",
-        projectId: "instagram-clone-41666",
-        storageBucket: "instagram-clone-41666.appspot.com",
+      options: FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_API_KEY']!,
+        appId: dotenv.env['FIREBASE_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
       ),
     );
   } else {
--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/utils/utis.dart';
 
 class AddPost extends StatefulWidget {
@@ -16,12 +17,11 @@ class AddPost extends StatefulWidget {
 class _AddPostState extends State<AddPost> {
   final ImagePicker _picker = ImagePicker();
 
-  File? selectedAvatar;
+  File? selectedPostImage;
 
   void pickImage(type) async {
     final image = await _picker.pickImage(
       source: type,
-      maxWidth: 400,
     );
 
     if (image == null) {
@@ -29,8 +29,10 @@ class _AddPostState extends State<AddPost> {
     }
 
     setState(() {
-      selectedAvatar = File(image.path);
+      selectedPostImage = File(image.path);
     });
+
+    context.pop();
   }
 
   Future selectImage(BuildContext context) {
@@ -45,7 +47,7 @@ class _AddPostState extends State<AddPost> {
                 pickImage(ImageSource.camera);
               },
               child: const Text(
-                "Open camera",
+                "Open Camera",
                 style: TextStyle(
                   color: Colors.blueAccent,
                   fontSize: 16,
@@ -57,7 +59,7 @@ class _AddPostState extends State<AddPost> {
                 pickImage(ImageSource.gallery);
               },
               child: const Text(
-                "Open gallery",
+                "Pick from Gallery",
                 style: TextStyle(
                   color: Colors.blueAccent,
                   fontSize: 16,
@@ -84,16 +86,49 @@ class _AddPostState extends State<AddPost> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: IconButton(
-        icon: const Icon(
-          Icons.upload,
-          size: 30,
-        ),
-        onPressed: () {
-          selectImage(context);
-        },
-      ),
-    );
+    return selectedPostImage != null
+        ? Center(
+            child: IconButton(
+              icon: const Icon(
+                Icons.upload,
+                size: 30,
+              ),
+              onPressed: () {
+                selectImage(context);
+              },
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              leading: BackButton(
+                onPressed: () {
+                  setState(() {
+                    selectedPostImage = null;
+                  });
+                },
+              ),
+              title: const Text(
+                "Post to",
+                style: TextStyle(
+                  color: primaryColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Post",
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 19,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
